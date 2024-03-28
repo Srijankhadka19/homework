3fe401c1f04a5fb1f692fe1903702111291ba1e7
Srijan khadka <99087250+Srijankhadka19@users.noreply.github.com>
2024-03-28 05:50:08 +0545

    create three variable and print them

---
+++ variable.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name="srijan";
+    echo"$name";
+    $age=23;
+    echo"$age";
+    $sem="sixth";
+    echo"$sem"
+    ?>
+</body>
+</html>
